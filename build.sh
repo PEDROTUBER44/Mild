176d23248e51f25ce4caa1b878563faf2d907a5b
PEDROTUBER44 <pedro.rosendo@protonmail.com>
2021-11-20 17:57:15 -0300

    Update 0.9.8: Update mild package

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ cargo build --release ;
 rm -rf app/* ;
 cp target/release/mild app/ ;
 rm -rf target/ ;
-cp app/mild dev/debbuild/mild_1.0_amd64/usr/bin/ ;
 echo "<<<>>>" ;
--- dev/debbuild/builddeb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-dpkg-deb -b mild_1.0_amd64/ mild_1.0.0_amd64.deb ;
-echo "Package mild .deb created";
